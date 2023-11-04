15454768f04138f362bede69bc629df9be0880b4
Aditya Pratap Singh <cybercreed010@gmail.com>
2023-11-04 12:37:48 +0530

    Requiremts

--- models/answer.py
+++ models/answer.py
@@ -2,9 +2,13 @@ from typing import List
 from pydantic import BaseModel
 
 
+class QuestionAnswerPair(BaseModel):
+    question: str
+    answer: str
+
 class AnswerEvaluationRequest(BaseModel):
-    qa_pairs: List[dict]
+    qa_pairs: List[QuestionAnswerPair]
 
 class AnswerEvaluationResponse(BaseModel):
     score: float
-    correct_answers: List[dict]
+    correct_answers: List[QuestionAnswerPair]
